d5ac7a4b223b29f46eee9cfcec30d13028053d56
chattrinu <chattrinu@gmail.com>
2023-05-08 19:51:38 -0400

    third commit

--- src/FoodMenu.java
+++ src/FoodMenu.java
@@ -1,5 +1,47 @@
 public class FoodMenu {
-    public String Name = "Rice";
+    private String Name = "Rice";
     private double Price = 10;
+    private int SpicyLavel;
+
+    public FoodMenu (String name, double price, int spicyLavel) {
+        Name = name;
+        Price = price;
+        SpicyLavel = spicyLavel;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        if(name.contains("money"))
+        {
+            System.out.println("not allowed");
+            return;
+        }
+        Name = name;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        if(price<1 || price > 1000) {
+            throw new RuntimeException();
+
+        }
+        Price = price;
+    }
+
+    public int getSpicyLavel() {
+        return SpicyLavel;
+    }
+
+    public void setSpicyLavel(int spicyLavel) {
+
+        SpicyLavel = spicyLavel;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        FoodMenu foodMenu = new FoodMenu();
-        System.out.println( "Food name is :"+foodMenu.Name);
+        FoodMenu foodMenu = new FoodMenu("Crab",10,0);
+        System.out.println( "Food name is :"+foodMenu.getName());
+        System.out.println( "Food name is :"+foodMenu.getPrice());
     }
 }
